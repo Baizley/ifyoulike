e2376443e26eaa4c72d632e56e8d2892c550ae6c
Baizley <Baizley@users.noreply.github.com>
2020-06-24 20:25:11 +0200

    Refactor the structure of the API methods.

--- src/main/java/com/baizley/ifyoulike/reddit/Reddit.java
+++ src/main/java/com/baizley/ifyoulike/reddit/Reddit.java
@@ -7,18 +7,15 @@ import com.baizley.ifyoulike.model.ResponseKind;
 import com.baizley.ifyoulike.model.SearchResult;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.List;
 import java.util.Objects;
@@ -43,80 +40,78 @@ public class Reddit {
     }
 
     public ResponseKind<Listing<SearchResult>> searchSubreddit(String searchTerm) throws IOException, InterruptedException{
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Authorization", accessToken.toHeader())
+                .header("User-Agent", USER_AGENT)
+                .uri(
+                    UriComponentsBuilder
+                            .fromHttpUrl("https://oauth.reddit.com/r/ifyoulikeblank/search.json?restrict_sr=true&q={searchTerm}")
+                            .encode()
+                            .buildAndExpand(searchTerm)
+                            .toUri()
+                )
+                .build();
+
         // TODO: Check status codes.
-        String body = httpClient.send(
-                HttpRequest.newBuilder()
-                        .header("Authorization", accessToken.toHeader())
-                        .header("User-Agent", USER_AGENT)
-                        .uri(
-                            UriComponentsBuilder
-                                    .fromHttpUrl("https://oauth.reddit.com/r/ifyoulikeblank/search.json?restrict_sr=true&q={searchTerm}")
-                                    .encode()
-                                    .buildAndExpand(searchTerm)
-                                    .toUri()
-                        )
-                        .build(),
-                HttpResponse.BodyHandlers.ofString()
-        ).body();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
         Type type = new TypeToken<ResponseKind<Listing<SearchResult>>>() {}.getType();
-
-        return new Gson().fromJson(body, type);
+        return new Gson().fromJson(response.body(), type);
     }
 
     public List<ResponseKind<Listing<Comment>>> fetchCommentTree(String articleId) throws URISyntaxException, IOException, InterruptedException {
-        String body = httpClient.send(
-                HttpRequest.newBuilder()
-                        .header("Authorization", accessToken.toHeader())
-                        .header("User-Agent", USER_AGENT)
-                        .uri(new URI("https://oauth.reddit.com/r/ifyoulikeblank/comments/" + articleId))
-                        .build(),
-                HttpResponse.BodyHandlers.ofString()
-        ).body();
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Authorization", accessToken.toHeader())
+                .header("User-Agent", USER_AGENT)
+                .uri(new URI("https://oauth.reddit.com/r/ifyoulikeblank/comments/" + articleId))
+                .build();
+
+        // TODO: Check status codes.
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
         Type type = new TypeToken<List<ResponseKind<Listing<Comment>>>>() {}.getType();
-        List<ResponseKind<Listing<Comment>>> comments = new Gson().fromJson(body, type);
-
-        return comments.stream()
-                .filter(response -> response.data()
-                        .children()
-                        .stream()
-                        .map(ResponseKind::data)
-                        .map(Comment::body)
-                        .anyMatch(Objects::nonNull)
+        List<ResponseKind<Listing<Comment>>> responseKinds = new Gson().fromJson(response.body(), type);
+        return responseKinds.stream()
+                .filter(kind ->
+                        kind.data()
+                            .children()
+                            .stream()
+                            .map(ResponseKind::data)
+                            .map(Comment::body)
+                            .anyMatch(Objects::nonNull)
                 )
                 .collect(Collectors.toList());
     }
 
     public AccessToken fetchAccessToken() {
         // TODO: Add logic for refreshing access token using refresh token.
-        if (accessToken != null) {
-            return accessToken;
+        if (this.accessToken != null) {
+            return this.accessToken;
         }
 
         try {
-            // TODO: Check status codes.
             HttpRequest accessTokenRequest = HttpRequest.newBuilder()
                     .header("Accept", "application/json")
                     .header("Authorization", "Basic " + AUTHORIZATION_HEADER)
                     .header("User-Agent", USER_AGENT)
                     .uri(new URI("https://www.reddit.com/api/v1/access_token"))
                     .POST(
-                            HttpRequest
-                                    .BodyPublishers
-                                    .ofString(
-                                            "grant_type=password" +
-                                                  "&username=" + username +
-                                                  "&password=" + password
-                                    )
+                        HttpRequest
+                            .BodyPublishers
+                            .ofString(
+                            "grant_type=password" +
+                                  "&username=" + username +
+                                  "&password=" + password
+                            )
                     )
                     .build();
 
+            // TODO: Check status codes.
             HttpResponse<String> response = httpClient.send(accessTokenRequest, HttpResponse.BodyHandlers.ofString());
 
             this.accessToken = new Gson().fromJson(response.body(), AccessToken.class);
 
-            return accessToken;
+            return this.accessToken;
         } catch (URISyntaxException | InterruptedException | IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
