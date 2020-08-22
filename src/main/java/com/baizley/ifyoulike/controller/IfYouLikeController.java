f3e4a7d9aac46f15495734e343ec6c3024cc237b
Baizley <Baizley@users.noreply.github.com>
2020-08-22 14:53:28 +0200

    Add word breaking to avoid overflowing the recommendation box.

--- src/main/java/com/baizley/ifyoulike/controller/IfYouLikeController.java
+++ src/main/java/com/baizley/ifyoulike/controller/IfYouLikeController.java
@@ -4,13 +4,17 @@ import com.baizley.ifyoulike.service.RecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
 
 @Controller
 public class IfYouLikeController {
@@ -28,11 +32,33 @@ public class IfYouLikeController {
     }
 
     @RequestMapping(value = "/ifyoulike{blank}")
-    public String ifYouLikeHtml(@PathVariable String blank, Model model) {
+    public SseEmitter ifYouLikeServerSentEvents(@PathVariable String blank) {
+        SseEmitter emitter = new SseEmitter();
 
-        model.addAttribute("blank", blank);
-        model.addAttribute("comments", recommendationService.retrieveRecommendations(blank));
+        List<String> recommendations = recommendationService.retrieveRecommendations(blank);
 
-        return "index";
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        service.submit(() -> {
+            for (String recommendation : recommendations) {
+                SseEmitter.SseEventBuilder name = SseEmitter.event()
+                        .data(recommendation)
+                        .name("recommendation");
+                try {
+                    emitter.send(name);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return emitter;
+    }
+
+
+
+    @RequestMapping(value = "/ifyoulike{blank}", produces = TEXT_HTML_VALUE)
+    public String ifYouLikeHtml() {
+        return "forward:/ifyoulike/index.html";
     }
 }
